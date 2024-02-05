1dec6a6178f320c19c19d48d758bccb88ce827aa
Abhishek <abhishekverma7t@gmail.com>
2024-02-05 17:55:22 +0530

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/global_variable.dart';
 import 'package:shop_app/home_page.dart';
+import 'package:shop_app/product_details_page.dart';
 
-void main(){
+void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Shop App',
       theme: ThemeData(
-        fontFamily: 'Poppins',
-        colorScheme: ColorScheme.fromSeed(
+          fontFamily: 'Poppins',
+          colorScheme: ColorScheme.fromSeed(
             seedColor: Colors.amber,
             primary: Colors.amber,
-        ),
-        inputDecorationTheme: const InputDecorationTheme(
-          hintStyle: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 16
           ),
-          prefixIconColor: Color.fromRGBO(119, 119, 119, 1),
-        ),
-        textTheme: const TextTheme(
-          titleMedium: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold
+          appBarTheme: const AppBarTheme(
+            centerTitle: true,
+            titleTextStyle: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            )
           ),
-          bodySmall: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 16,
-          )
-        )
-      ),
+          useMaterial3: true,
+          inputDecorationTheme: const InputDecorationTheme(
+            hintStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+            prefixIconColor: Color.fromRGBO(119, 119, 119, 1),
+          ),
+          textTheme: const TextTheme(
+              titleMedium: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              bodySmall: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ))),
       home: const HomePage(),
     );
   }
---
+++ lib/product_details_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ProductDetailsPage extends StatelessWidget {
+
+  final Map<String, Object> product;
+
+  const ProductDetailsPage({
+    super.key,
+    required this.product
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Details'),
+      ),
+      body: Column(
+        children: [
+          Text(product['title'] as String),
+        ],
+      ),
+    );
+  }
+}
