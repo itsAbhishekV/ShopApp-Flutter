01553ec57a3f90ca8ac177fe41e0aa46dc86a5d8
Abhishek <abhishekverma7t@gmail.com>
2024-02-05 15:04:10 +0530

    added hardcoded data for shoe cards and added image assets

---
+++ lib/global_variable.dart
@@ -0,0 +1,34 @@
+final products = [
+  {
+    'id': '0',
+    'title': 'Men\'s Nike Shoes',
+    'price': 44.52,
+    'imageUrl': 'assets/images/air-max-90-shoes-0MB5rJ.png',
+    'company': 'Nike',
+    'sizes': [9, 10, 11, 12],
+  },
+  {
+    'id': '1',
+    'title': 'Adidas Shoes',
+    'price': 20.12,
+    'imageUrl': 'assets/images/adidas-originals-shoes.png',
+    'company': 'Adidas',
+    'sizes': [9, 10, 12],
+  },
+  {
+    'id': '2',
+    'title': 'Bata Women\'s Shoes',
+    'price': 28.95,
+    'imageUrl': 'assets/images/bata-women-shoes.png',
+    'company': 'Bata',
+    'sizes': [8, 9, 10],
+  },
+  {
+    'id': '3',
+    'title': 'Jordan Shoes',
+    'price': 420.69,
+    'imageUrl': 'assets/images/air-jordan-red.png',
+    'company': 'Nike',
+    'sizes': [8, 9, 10],
+  },
+];
