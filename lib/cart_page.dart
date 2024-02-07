08090c4689fb3ab25fc90bf2e2348b07123f6a98
crossword <crossword@crosswords-iMac.local>
2024-02-07 17:38:52 +0530

    added cart item add and remove functionality

--- lib/cart_page.dart
+++ lib/cart_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shop_app/global_variable.dart';
 
 import 'cart_provider.dart';
 
@@ -15,7 +14,12 @@ class CartPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Cart'),
       ),
-      body: ListView.builder(
+      body: cart.isEmpty ? const Center(
+        child: Text("The Cart is Empty  :'(",
+        style: TextStyle(
+          color: Colors.black26,
+        ),)
+      ) : ListView.builder(
           itemCount: cart.length,
           itemBuilder: (BuildContext context, int index){
             final cartItem = cart[index];
@@ -26,7 +30,31 @@ class CartPage extends StatelessWidget {
               trailing: IconButton(onPressed: (){
                 showDialog(context: context, builder: (context) {
                   return AlertDialog(
-                    title: Text('Delete Product', style: Theme.of(context).textTheme.titleMedium,)
+                    title: Text('Delete Product', style: Theme.of(context).textTheme.titleMedium,),
+                    content: Text('Are you sure you want to remove ${cartItem['title']} from Cart?'),
+                    actions: [
+                      TextButton(
+                          onPressed: (){
+                            Navigator.of(context).pop();
+                  },
+                          child: const Text('No', style: TextStyle(
+                            color: Colors.blue,
+                            fontWeight: FontWeight.bold
+                          ),
+                          )
+                      ),
+                      TextButton(
+                          onPressed: () {
+                            Provider.of<CartProvider>(context, listen: false).removeProduct(cartItem);
+                            Navigator.of(context).pop();
+                          },
+                          child: const Text('Yes', style: TextStyle(
+                              color: Colors.red,
+                              fontWeight: FontWeight.bold
+                          ),
+                          )
+                      ),
+                    ],
                   );
                 }
                 );
